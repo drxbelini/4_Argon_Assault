08f703850c2e48cf39e4636bd9ba1be5a26b21f1
DESKTOP-4QB4TVC\Diego Belini <drxbelini@gmail.com>
2019-03-15 23:06:25 -0300

    Organized assets Included model guizão alteraded scrit better way point on camera obj

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke ("loadScene", 4F);
+    }
+
+    // Update is called once per frame
+    void loadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
